33c5bb7e2d06b580c703adc07dbb3fcadae51e5e
as-f <as-f@users.noreply.github.com>
2017-06-20 15:25:40 -0700

    Make pdf preview image scollable

--- app/src/main/java/io/github/as_f/barpager/SliceImageView.kt
+++ app/src/main/java/io/github/as_f/barpager/SliceImageView.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.ImageView
 
 class SliceImageView(context: Context?, attrs: AttributeSet?) : ImageView(context, attrs) {
 
+  var scrollLength = 0
 
   override fun setImageBitmap(bm: Bitmap?) {
+    val values = FloatArray(9)
+    imageMatrix.getValues(values)
+    Log.v("Matrix", imageMatrix.toString())
     super.setImageBitmap(bm)
   }
 
