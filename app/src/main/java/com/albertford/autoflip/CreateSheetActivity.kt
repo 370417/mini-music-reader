29e250320bbb33c175db0021d34ce25a2f0255ae
as-f <as-f@users.noreply.github.com>
2017-07-24 21:52:06 -0700

    Improve create sheet layouts

--- app/src/main/java/com/albertford/autoflip/CreateSheetActivity.kt
+++ app/src/main/java/com/albertford/autoflip/CreateSheetActivity.kt
@@ -7,16 +7,25 @@ import android.util.AttributeSet
 import android.view.Menu
 import android.view.View
 import com.albertford.autoflip.R
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_create_sheet.*
 
 class CreateSheetActivity : AppCompatActivity() {
 
+  lateinit var realm: Realm
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    realm = Realm.getDefaultInstance()
     setContentView(R.layout.activity_create_sheet)
 
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    realm.close()
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
