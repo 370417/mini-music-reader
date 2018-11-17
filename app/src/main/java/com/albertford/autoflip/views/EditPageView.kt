69e3544997f630d740c423b8af8a0941d62d9c11
as-f <as-f@users.noreply.github.com>
2018-11-16 20:25:52 -0600

    Prevent recyclerview from intercepting motion events

--- app/src/main/java/com/albertford/autoflip/views/EditPageView.kt
+++ app/src/main/java/com/albertford/autoflip/views/EditPageView.kt
@@ -1,8 +1,10 @@
 package com.albertford.autoflip.views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 private val identityMatrix = Matrix()
@@ -21,4 +23,26 @@ class EditPageView(context: Context?, attrs: AttributeSet) : View(context, attrs
         val bitmap = bitmap ?: return
         canvas.drawBitmap(bitmap, identityMatrix, null)
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return true
+    }
 }
+
+private class Point
+
+private sealed class Selection
+
+private class InitialBarSelection(val origin: Int, var drag: Int) : Selection()
+
+
+
+// Initial bar selection
+// inital bar corner resize
+// initial bar edge resize
+
+// once the inital bar is created, the bottom sheet buttons will pop up:
+// Add bar / Add sheet (or Next Bar / Next Sheet for when the bar is not the current last bar)
+
+// Use barlines and staff classes instead of bar classes?
---
+++ app/src/main/java/com/albertford/autoflip/views/PoliteRecyclerView.kt
@@ -0,0 +1,26 @@
+package com.albertford.autoflip.views
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+/**
+ * A RecyclerView that only intercepts touch events if multiple pointers are used.
+ *
+ * This means:
+ *  - nested views can still receive move events
+ *  - nested views do not have to worry about multiple pointers
+ *  - the user can still always scroll the recyclerview by using two fingers
+ */
+class PoliteRecyclerView(context: Context, attrs: AttributeSet?) : RecyclerView(context, attrs) {
+
+    override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
+        e ?: return false
+        return if (e.pointerCount == 1) {
+            false
+        } else {
+            super.onInterceptTouchEvent(e)
+        }
+    }
+}
