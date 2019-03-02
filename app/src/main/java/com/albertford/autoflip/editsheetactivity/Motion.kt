880ea97fd46f371521cd62dbe9a8157f73e16519
as-f <as-f@users.noreply.github.com>
2019-03-01 18:26:44 -0600

    Add bounds checking to resizing

--- app/src/main/java/com/albertford/autoflip/editsheetactivity/Motion.kt
+++ app/src/main/java/com/albertford/autoflip/editsheetactivity/Motion.kt
@@ -6,6 +6,8 @@ import com.albertford.autoflip.room.BarLine
 import com.albertford.autoflip.room.Page
 import com.albertford.autoflip.room.Staff
 
+const val STAFF_OVERLAP = 0.05f
+
 // touch is always a PointF and never just a Float. This is because, even when we are only resizing
 // in one dimension, the other dimension is useful for knowing when to snap to other lines
 sealed class Motion(var touch: PointF) {
@@ -13,12 +15,6 @@ sealed class Motion(var touch: PointF) {
 
     abstract fun onActionMove(page: Page, selection: Selection?, slop: Float)
 
-    /**
-     * Returns the new selection if it needs to be changed. The new selection can be invalid to
-     * indicate the selection should be canceled.
-     * Returns the current selection if it has been clicked.
-     * Returns null if no changes to selection need to be made
-     */
     abstract fun onActionUp(page: Page, selection: Selection?, slop: Float): MotionResult?
 }
 
@@ -36,29 +32,6 @@ class ClickSelection(touch: PointF) : Motion(touch) {
     }
 }
 
-fun destroyBarIfTooSmall(staff: Staff, barIndex: Int, slop: Float, fixedX: Float): MotionResult? {
-    val prevBarLine = staff.barLines.getOrNull(barIndex - 1)
-    val leftBarLine = staff.barLines[barIndex]
-    val rightBarLine = staff.barLines[barIndex + 1]
-    val nextBarLine = staff.barLines.getOrNull(barIndex + 2)
-    if (prevBarLine != null && leftBarLine.x - prevBarLine.x < slop) {
-        staff.barLines.removeAt(barIndex)
-        return CancelSelectionResult
-    } else if (rightBarLine.x - leftBarLine.x < slop) {
-        // delete the barline that was just moved, not the fixed one
-        if (rightBarLine.x == fixedX) {
-            staff.barLines.removeAt(barIndex)
-        } else {
-            staff.barLines.removeAt(barIndex + 1)
-        }
-        return CancelSelectionResult
-    } else if (nextBarLine != null && nextBarLine.x - rightBarLine.x < slop) {
-        staff.barLines.removeAt(barIndex + 1)
-        return CancelSelectionResult
-    }
-    return null
-}
-
 class ResizeCorner(touch: PointF, private val fixedCorner: PointF) : Motion(touch) {
     override fun onActionMove(page: Page, selection: Selection?, slop: Float) {
         val selection = selection ?: return
@@ -68,13 +41,13 @@ class ResizeCorner(touch: PointF, private val fixedCorner: PointF) : Motion(touc
         val right = Math.max(touch.x, fixedCorner.x)
         val bottom = Math.max(touch.y, fixedCorner.y)
         val rect = RectF(left, top, right, bottom)
-        val clamped = rect
-        staff.top = clamped.top
-        staff.bottom = clamped.bottom
+        clampBar(rect, page, selection)
+        staff.top = rect.top
+        staff.bottom = rect.bottom
         val leftBar = staff.barLines[selection.barIndex]
         val rightBar = staff.barLines[selection.barIndex + 1]
-        leftBar.x = clamped.left
-        rightBar.x = clamped.right
+        leftBar.x = rect.left
+        rightBar.x = rect.right
     }
 
     override fun onActionUp(page: Page, selection: Selection?, slop: Float): MotionResult? {
@@ -96,11 +69,11 @@ class ResizeHorizontal(touch: PointF, private val fixedX: Float) : Motion(touch)
         val left = Math.min(touch.x, fixedX)
         val right = Math.max(touch.x, fixedX)
         val rect = RectF(left, staff.top, right, staff.bottom)
-        val clamped = rect
-        val leftBar = staff.barLines[selection.barIndex]
-        val rightBar = staff.barLines[selection.barIndex + 1]
-        leftBar.x = clamped.left
-        rightBar.x = clamped.right
+        clampBarHorizontal(rect, page, selection)
+        val leftBarLine = staff.barLines[selection.barIndex]
+        val rightBarLine = staff.barLines[selection.barIndex + 1]
+        leftBarLine.x = rect.left
+        rightBarLine.x = rect.right
     }
 
     override fun onActionUp(page: Page, selection: Selection?, slop: Float): MotionResult? {
@@ -119,9 +92,9 @@ class ResizeVertical(touch: PointF, private val fixedY: Float) : Motion(touch) {
         val top = Math.min(touch.y, fixedY)
         val bottom = Math.max(touch.y, fixedY)
         val rect = RectF(leftBar.x, top, rightBar.x, bottom)
-        val clamped = rect
-        staff.top = clamped.top
-        staff.bottom = clamped.bottom
+        clampBarVertical(rect, page, selection)
+        staff.top = rect.top
+        staff.bottom = rect.bottom
     }
 
     override fun onActionUp(page: Page, selection: Selection?, slop: Float): MotionResult? {
@@ -227,3 +200,54 @@ class ChangeSelectionResult(val newSelection: Selection) : MotionResult()
 object ClickSelectionResult : MotionResult()
 
 object CancelSelectionResult : MotionResult()
+
+fun destroyBarIfTooSmall(staff: Staff, barIndex: Int, slop: Float, fixedX: Float): MotionResult? {
+    val prevBarLine = staff.barLines.getOrNull(barIndex - 1)
+    val leftBarLine = staff.barLines[barIndex]
+    val rightBarLine = staff.barLines[barIndex + 1]
+    val nextBarLine = staff.barLines.getOrNull(barIndex + 2)
+    if (prevBarLine != null && leftBarLine.x - prevBarLine.x < slop) {
+        staff.barLines.removeAt(barIndex)
+        return CancelSelectionResult
+    } else if (rightBarLine.x - leftBarLine.x < slop) {
+        // delete the barline that was just moved, not the fixed one
+        if (rightBarLine.x == fixedX) {
+            staff.barLines.removeAt(barIndex)
+        } else {
+            staff.barLines.removeAt(barIndex + 1)
+        }
+        return CancelSelectionResult
+    } else if (nextBarLine != null && nextBarLine.x - rightBarLine.x < slop) {
+        staff.barLines.removeAt(barIndex + 1)
+        return CancelSelectionResult
+    }
+    return null
+}
+
+fun clampBar(rect: RectF, page: Page, selection: Selection) {
+    clampBarVertical(rect, page, selection)
+    clampBarHorizontal(rect, page, selection)
+}
+
+fun clampBarVertical(rect: RectF, page: Page, selection: Selection) {
+    val prevStaff = page.staves.getOrNull(selection.staffIndex - 1)
+    val nextStaff = page.staves.getOrNull(selection.staffIndex + 1)
+    if (prevStaff != null && rect.top < prevStaff.bottom - STAFF_OVERLAP) {
+        rect.top = prevStaff.bottom - STAFF_OVERLAP
+    }
+    if (nextStaff != null && rect.bottom > nextStaff.top + STAFF_OVERLAP) {
+        rect.bottom = nextStaff.top + STAFF_OVERLAP
+    }
+}
+
+fun clampBarHorizontal(rect: RectF, page: Page, selection: Selection) {
+    val staff = page.getStaff(selection)
+    val prevBarLine = staff.barLines.getOrNull(selection.barIndex - 1)
+    val nextBarLine = staff.barLines.getOrNull(selection.barIndex + 2)
+    if (prevBarLine != null && rect.left < prevBarLine.x) {
+        rect.left = prevBarLine.x
+    }
+    if (nextBarLine != null && rect.right > nextBarLine.x) {
+        rect.right = nextBarLine.x
+    }
+}
